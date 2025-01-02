afa364eaf5e3c3d6624470aa73c86a1799214a6c
Oleg Kmit <oleg.kmit@gmail.com>
2025-01-02 18:07:32 +0200

    Add implementation for add method in sections controller

--- app/controllers/api/v1/students/sections_controller.rb
+++ app/controllers/api/v1/students/sections_controller.rb
@@ -2,9 +2,14 @@ module Api
   module V1
     module Students
       class SectionsController < ApplicationController
-        # TODO
         def add
-          render json: {}, status: :created
+          result = Sections::AddService.new(section_id: params[:section_id], student_id: params[:student_id]).call
+
+          if result[:success]
+            render json: result[:data], status: :created
+          else
+            render json: { errors: result[:errors] }, status: :unprocessable_entity
+          end
         end
 
         # TODO
