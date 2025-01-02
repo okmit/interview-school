df14fde082944c16d27e9907b1d657ffb9a2bbaf
Oleg Kmit <oleg.kmit@gmail.com>
2025-01-02 15:29:22 +0200

    Add section extra validation for duration and range

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,7 +1,57 @@
 class Section < ApplicationRecord
+  VALID_DAYS = %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday].freeze
+  EARLIEST_START_TIME_MINUTES = 7 * 60 + 30 # 7:30 AM
+  LATEST_END_TIME_IN_MINUTES = 22 *  60 # 10:00 PM
+  VALID_DURATION_IN_MINUTES = [ 50, 80 ]
+
   belongs_to :teacher
   belongs_to :subject
   belongs_to :classroom
 
   validates :start_time, :end_time, :days, presence: true
+
+  validate :validate_time_range
+  validate :validate_duration
+  validate :validate_days
+
+  private
+
+  def validate_time_range
+    return if start_time.blank? || end_time.blank? || !start_time.is_a?(Time) || !end_time.is_a?(Time)
+
+    start_time_in_minutes = start_time.hour * 60 + start_time.min
+    end_time_in_minutes = end_time.hour * 60 + end_time.min
+
+    if start_time_in_minutes < EARLIEST_START_TIME_MINUTES || start_time_in_minutes > LATEST_END_TIME_IN_MINUTES
+      errors.add(:start_time, "must be between 7:30 AM and 10:00 PM")
+    end
+
+    if end_time_in_minutes < EARLIEST_START_TIME_MINUTES || end_time_in_minutes > LATEST_END_TIME_IN_MINUTES
+      errors.add(:end_time, "must be between 7:30 AM and 10:00 PM")
+    end
+
+    if start_time >= end_time
+      errors.add(:end_time, "must be after start time")
+    end
+  end
+
+  def validate_duration
+    return if start_time.blank? || end_time.blank? || !start_time.is_a?(Time) || !end_time.is_a?(Time)
+
+    duration_in_minutes = ((end_time - start_time) / 60).to_i # time in mins = sec / 60 sec per min
+
+    unless VALID_DURATION_IN_MINUTES.include?(duration_in_minutes)
+      errors.add(:duration, "Section duration must be either 50 or 80 minutes")
+    end
+  end
+
+  def validate_days
+    return if days.blank? || !days.is_a?(Array)
+
+    invalid_days = days - VALID_DAYS
+
+    if invalid_days.any?
+      errors.add(:days, "contains invalid days: #{invalid_days.join(', ')}")
+    end
+  end
 end
--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -1,15 +1,75 @@
 require 'rails_helper'
 
 RSpec.describe Section, type: :model do
-  describe 'validations' do
-    it { should validate_presence_of :start_time }
-    it { should validate_presence_of :end_time }
-    it { should validate_presence_of :days }
-  end
+  let(:teacher) { FactoryBot.create(:teacher) }
+  let(:classroom) { FactoryBot.create(:classroom) }
+  let(:subject) { FactoryBot.create(:subject) }
 
-  describe 'associations' do
-    it { should belong_to(:teacher) }
-    it { should belong_to(:subject) }
-    it { should belong_to(:classroom) }
+  describe 'validations' do
+    context "with valid attributes" do
+      it "is valid" do
+        section = Section.new({
+          teacher: teacher,
+          subject: subject,
+          classroom: classroom,
+          start_time: Time.zone.parse("7:30 AM"),
+          end_time: Time.zone.parse("8:20 AM"),
+          days: [ "Monday", "Friday" ]
+        })
+
+        expect(section).to be_valid
+      end
+    end
+
+    context "with invalid attributes" do
+      it 'duration is invalid' do
+        section = Section.new({
+          teacher: teacher,
+          subject: subject,
+          classroom: classroom,
+          start_time: Time.zone.parse("7:30 AM"),
+          end_time: Time.zone.parse("8:21 AM"),
+          days: [ "Monday" ]
+        })
+
+        expect(section).not_to be_valid
+        expect(section.errors[:duration]).to include("Section duration must be either 50 or 80 minutes")
+      end
+
+      it 'start time and end time is invalid' do
+        section = Section.new({
+          teacher: teacher,
+          subject: subject,
+          classroom: classroom,
+          start_time: nil,
+          end_time: '',
+          days: [ "Monday" ]
+        })
+
+        expect(section).not_to be_valid
+        expect(section.errors[:start_time]).to include("can't be blank")
+        expect(section.errors[:end_time]).to include("can't be blank")
+      end
+
+
+      it 'is invalid' do
+        section = Section.new({
+          teacher: nil,
+          subject: nil,
+          classroom: nil,
+          start_time: Time.zone.parse("6:30 AM"),
+          end_time: Time.zone.parse("22:20"),
+          days: [ "Fr", "Someday" ]
+        })
+
+        expect(section).not_to be_valid
+        expect(section.errors[:teacher]).to include("must exist")
+        expect(section.errors[:subject]).to include("must exist")
+        expect(section.errors[:classroom]).to include("must exist")
+        expect(section.errors[:start_time]).to include("must be between 7:30 AM and 10:00 PM")
+        expect(section.errors[:end_time]).to include("must be between 7:30 AM and 10:00 PM")
+        expect(section.errors[:days]).to include("contains invalid days: Fr, Someday")
+      end
+    end
   end
 end
